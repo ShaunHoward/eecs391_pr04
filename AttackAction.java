d475833da4695a2a58084a3aff42960b077487c3
ShaunHoward <smh150@case.edu>
2015-04-17 04:32:36 -0400

    Good stuff but learns poorly and reward grows too large

---
+++ AttackAction.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Attack action class that simply stores the map of actions that deal with attacking
+ * for the player's footmen.
+ * 
+ * @author Shaun Howard (smh150)
+ *
+ */
+public class AttackAction {
+	private Map<Integer, Integer> attack = new HashMap<Integer, Integer>();
+	
+	public AttackAction(Map<Integer, Integer> attack) {
+		this.attack = new HashMap<Integer, Integer>(attack);
+	}
+
+	public Map<Integer, Integer> getAttack() {
+		return attack;
+	}
+	
+}
